abaa28ef3691a1aa8c673df244b2733685289f63
Yaroslav <jarosloov@yandex.ru>
2020-09-30 13:28:40 +0500

    :)

---
+++ С++/Самая сложная программа.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+    cout << "Help" << endl; // Я не понимаю, что я делаю
+    cout << "first" << endl;
+    cout << "second" << endl;
+    return 0; 
+}
+
+// надеюсь всё получится 
