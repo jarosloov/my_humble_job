9fc4e947ea6af6f8f5e33aeaca1db77593f7d737
Yaroslav <jarosloov@yandex.ru>
2020-10-03 17:03:43 +0500

    добовление проигрыша

---
+++ Unity/Проекты/First/Assets/skripts/Touching_the_ground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// SceneManager. LoadScene("имя сцены")
+
+public class Touching_the_ground : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+         if (other.gameObject.CompareTag("ground"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+
+
+}
